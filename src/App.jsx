b9a2e9cc7943cfb6a007e9b81c17757880a71299
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-10-20 16:37:11 -0300

    adding months, subtotal and total

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,62 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  
+
+  const [description, setDescription ] = useState("supermarket");
+  const [january, setJanuary] = useState(150);
+  const [february, setFebruary] = useState(160); 
+  const [items, setItems ] = useState([]) ;
+
+  const addItems = () => {
+    const subTotal = parseInt(january + february)
+      const newItem = {
+      description, 
+      january, 
+      february,
+     subTotal,
+    }
+    setItems([...items, newItem])
+  }
+
+  const calculateExpenses = () => {
+    let totalExpenses = 0;
+    items.forEach((item) => {
+      totalExpenses += item.subTotal;
+    })
+    return totalExpenses
+  }
   return (
     <div className="container">
+      <h1> Yearly Expenses Calculator</h1>
       <div className="labels">
         <label htmlFor="name">
-        Expense:
-          <input type="text" id="description" />
+          <p>Description:</p>
+          <input 
+          value={description}
+          onChange={(e)=> setDescription(e.target.value)}
+          type="text" id="description" placeholder="Example: Rent" />
         </label>
         <br />
         <label htmlFor="january">
-        January:
-          <input type="number" id="january" />
+          <p>January:</p>
+          <input type="number"
+          value={january}
+          onChange={(e) => setJanuary(e.target.value)}
+          id="january" />
         </label>
         <br />
         <label htmlFor="february">
-        February:
-          <input type="number" id="february" />
+          <p>February:</p>
+          <input type="number" 
+          value={february}
+          onChange={(e) => setFebruary(e.target.value)}
+          id="february" />
         </label>
         <br />
-        <label htmlFor="march">
-        March:
-          <input type="number" id="march" />
-        </label>
-        <br />
-        <label htmlFor="abril">
-        Abril:
-          <input type="number" id="abril" />
-        </label>
-        <br />
-        <label htmlFor="may">
-        May:
-          <input type="number" id="may" />
-        </label>
-        <br />
-        <button>Add</button>
+        <button 
+          onClick={addItems}
+          className="add-button">Add</button>
       </div>
       <div className="table">
         <table>
@@ -44,26 +65,23 @@ function App() {
               <th>Expense</th>
               <th>January</th>
               <th>February</th>
-              <th>March</th>
-              <th>Abril</th>
-              <th>May</th>
-              <th>Year</th>              
+              <th>Year Sub Total</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Rent</td>
-              <td>{0}</td>
-              <td>{0}</td>
-              <td>{0}</td>
-              <td>{0}</td>
-              <td>{0}</td>
-              <td>{0}</td>              
+            {items.map((item, index) =>(
+
+              <tr key={index}>
+              <td>{item.description}</td>
+              <td>{item.january}</td>
+              <td>{item.february}</td>
+              <td>{parseInt(item.january + item.february)}</td>
             </tr>
+              ))}
           </tbody>
         </table>
         <p>
-          Total: <span>{}</span>
+          Year Total: <span>{calculateExpenses()}</span>
         </p>
       </div>
     </div>
